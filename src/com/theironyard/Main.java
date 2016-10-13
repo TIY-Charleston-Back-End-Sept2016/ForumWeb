08f8594d9bf1f16981c642016939227c5f076f32
oakes <zsoakes@gmail.com>
2016-10-13 12:05:48 -0400

    Plug in SQL methods to the code

--- src/com/theironyard/Main.java
+++ src/com/theironyard/Main.java
@@ -1,5 +1,6 @@
 package com.theironyard;
 
+import org.h2.tools.Server;
 import spark.ModelAndView;
 import spark.Session;
 import spark.Spark;
@@ -10,9 +11,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Main {
-    static HashMap<String, User> users = new HashMap<>();
-    static ArrayList<Message> messages = new ArrayList<>();
-
     public static void createTables(Connection conn) throws SQLException {
         Statement stmt = conn.createStatement();
         stmt.execute("CREATE TABLE IF NOT EXISTS users (id IDENTITY, name VARCHAR, password VARCHAR)");
@@ -73,17 +71,12 @@ public class Main {
         return replies;
     }
 
-    public static void main(String[] args) {
-	    users.put("Alice", new User("Alice", "pass"));
-        users.put("Bob", new User("Bob", "pass"));
-        users.put("Charlie", new User("Charlie", "pass"));
+    public static void main(String[] args) throws SQLException {
+        Server.createWebServer().start();
+        Connection conn = DriverManager.getConnection("jdbc:h2:./main");
+        createTables(conn);
 
-        messages.add(new Message(0, -1, "Alice", "Hello everyone!"));
-        messages.add(new Message(1, -1, "Bob", "This is another thread!"));
-        messages.add(new Message(2, 0, "Charlie", "Cool thread, Alice."));
-        messages.add(new Message(3, 2, "Alice", "Thanks, Charlie."));
-
-        Spark.get(
+	    Spark.get(
                 "/",
                 (request, response) -> {
                     String replyId = request.queryParams("replyId");
@@ -96,12 +89,7 @@ public class Main {
                     String name = session.attribute("loginName");
 
                     HashMap m = new HashMap();
-                    ArrayList<Message> msgs = new ArrayList<Message>();
-                    for (Message message : messages) {
-                        if (message.replyId == replyIdNum) {
-                            msgs.add(message);
-                        }
-                    }
+                    ArrayList<Message> msgs = selectReplies(conn, replyIdNum);
                     m.put("messages", msgs);
                     m.put("name", name);
                     m.put("replyId", replyIdNum);
@@ -115,10 +103,9 @@ public class Main {
                 (request, response) -> {
                     String name = request.queryParams("loginName");
                     String pass = request.queryParams("password");
-                    User user = users.get(name);
+                    User user = selectUser(conn, name);
                     if (user == null) {
-                        user = new User(name, pass);
-                        users.put(name, user);
+                        insertUser(conn, name, pass);
                     }
                     else if (!pass.equals(user.password)) {
                         Spark.halt(403);
@@ -138,8 +125,21 @@ public class Main {
                     int replyId = Integer.valueOf(request.queryParams("replyId"));
                     Session session = request.session();
                     String name = session.attribute("loginName");
-                    Message msg = new Message(messages.size(), replyId, name, text);
-                    messages.add(msg);
+                    User user = selectUser(conn, name);
+                    if (user == null) {
+                        throw new Exception("Not logged in!");
+                    }
+                    insertMessage(conn, replyId, text, user.id);
+                    response.redirect(request.headers("Referer"));
+                    return null;
+                }
+        );
+
+        Spark.post(
+                "/logout",
+                (request, response) -> {
+                    Session session = request.session();
+                    session.invalidate();
                     response.redirect(request.headers("Referer"));
                     return null;
                 }
